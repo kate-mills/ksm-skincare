ee4a56472a495ba1934eafe0b357f81fc1fefff5
Kate Mills <kate.mills1124@gmail.com>
2020-06-23 14:50:33 -0700

    pages dir

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './homepage.styles.scss'
+
+const HomePage = () => {
+  return (
+    <div>
+      HomePage = KSM - Skincare
+    </div>
+  )
+}
+export default HomePage
